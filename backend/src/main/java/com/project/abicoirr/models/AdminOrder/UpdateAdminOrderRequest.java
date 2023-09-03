00f482fb04b988c98075bd95546f1254da7d9990
Rahul krishnan <rahulkrishnanr016@gmail.com>
2023-09-03 08:33:10 +0530

    removed jackson unwanted annotation

--- backend/src/main/java/com/project/abicoirr/models/AdminOrder/UpdateAdminOrderRequest.java
+++ backend/src/main/java/com/project/abicoirr/models/AdminOrder/UpdateAdminOrderRequest.java
@@ -1,6 +1,5 @@
 package com.project.abicoirr.models.AdminOrder;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,5 +7,4 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdateAdminOrderRequest extends CreateAdminOrderRequest {}
