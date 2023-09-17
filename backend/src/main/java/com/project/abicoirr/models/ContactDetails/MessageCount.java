17dcbe526d693c123a80a57b3c192191aefa9dce
Rahul krishnan <rahulkrishnanr016@gmail.com>
2023-09-17 18:43:09 +0530

    Message count response class import fix

--- backend/src/main/java/com/project/abicoirr/models/ContactDetails/MessageCount.java
+++ backend/src/main/java/com/project/abicoirr/models/ContactDetails/MessageCount.java
@@ -1,6 +1,10 @@
 package com.project.abicoirr.models.ContactDetails;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
